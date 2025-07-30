088600c01e5fc27839bc79e8c2d1aa8cd706b991
Ranjanunicode <ranjanamp6@gmail.com>
2025-07-31 01:21:57 +0530

    added indx and app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import URLExtractorApp from './components/URLExtractorApp';
+
+
+function App() {
+  return (
+    <div className="App">
+        <URLExtractorApp/>
+    </div>
+  );
+}
+
+export default App;
